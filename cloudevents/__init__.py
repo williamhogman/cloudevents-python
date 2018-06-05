61940e0f304aba7365aff2abaa41d4c193ed04ca
William Rudenmalm <me@whn.se>
2018-06-05 20:29:08 +0200

    add `WebhookDestination` to root of the module

--- cloudevents/__init__.py
+++ cloudevents/__init__.py
@@ -1,8 +1,9 @@
 """Cloudevents for Python
 """
 
-__all__ = ["create", "parse", "serialize"]
+__all__ = ["create", "parse", "serialize", "WebhookDestination"]
 
 from .construct import create
 from .parse import parse
 from .serialize import serialize
+from .webhook import WebhookDestination
